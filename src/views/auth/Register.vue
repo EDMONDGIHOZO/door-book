bfc3ae603c847fd7cbff07501d2ba7350f3ee397
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-04-07 01:46:40 +0200

    some appearance work on register page

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -2,7 +2,15 @@
   <div class="register">
     <v-row no-gutters>
       <v-col cols="5" class="hidden-sm-and-down">
-        <div class="comp form-container"></div>
+        <div class="comp form-container">
+          <v-img
+            :src="logo"
+            class="logo"
+            max-width="150"
+            max-height="150"
+          ></v-img>
+          <h1>DOORBOOK</h1>
+        </div>
       </v-col>
       <v-col cols="12" md="7">
         <div class="comp">
@@ -12,3 +20,30 @@
     </v-row>
   </div>
 </template>
+
+<script>
+export default {
+  name: "register",
+  computed: {
+    logo() {
+      return this.$store.state.mainLogo;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  animation: rotate 3.5s linear infinite;
+}
+
+.logo:hover{
+  animation: none;
+}
+
+@keyframes rotate {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
